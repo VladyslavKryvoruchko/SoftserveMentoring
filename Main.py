49268fe9feb90711f5914f4fe06fbab0180e20aa
VladyslavKryvoruchko <40231183+VladyslavKryvoruchko@users.noreply.github.com>
2022-05-11 11:12:20 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,10 +2,8 @@ from abc import ABC, abstractmethod
 from collections import namedtuple
 from pyllist import dllist, dllistnode
 
-# base definition of Point
 Point = namedtuple('Point', ['x', 'y', 'z'], defaults=[0,0,0])
 
-# base class for different storages
 class Storage(ABC):
     
     @staticmethod
@@ -27,59 +25,90 @@ class Storage(ABC):
     def put(self, *args, **kwargs):
         pass
 
-# class for Last 10 used (with duplicates)
+
 class LRUCache(Storage):
+    maxlen = 0
+    fastSaveStorage = dllist()
+    fastAccessStorage = dict()
     
     def __init__(self, size = 10):
         self.maxlen = size
-        self.storage = dllist()
+        self.fastSaveStorage = dllist()
+        self.fastAccessStorage = dict()
         
     @Storage.convert_params
-    def put(self, p):
-        if len(self.storage) >= self.maxlen:     # O(1)
-            self.storage.popleft()               # O(1)
-        self.storage.append(p)                   # O(1)
-            
-    @Storage.convert_params
-    def get(self, p):
-        if p not in self.storage:                # O(n)
-            print('{0} not found!'.format(p))          
+    def put(self, p: Point):
+        node = self.fastAccessStorage.get(p,False)
+        
+        if not node: 
+            if len(self.fastSaveStorage) >= self.maxlen:
+                self.fastAccessStorage.pop(self.fastSaveStorage.popleft())
+                
+            self.fastSaveStorage.append(p)
+            self.fastAccessStorage[p] = self.fastSaveStorage.last
         else:
-            return p
+            print('{0} already exists!'.format(p))
+            
+    
+    @Storage.convert_params
+    def get(self, p: Point):
+        elem = Point()
+        try:
+            elem = self.fastAccessStorage[p]
+            self.fastSaveStorage.remove(elem)
+            self.fastSaveStorage.append(p)
+        except KeyError: 
+            print('{0} key not found!'.format(p))
+        except ValueError:
+            print('{0} node not found!'.format(p))
+        except Exception as e:
+            print("Some other issue found!",e) 
+               
+        return elem
         
-
-# class for Top 10 frequently used
-class LFUCache(Storage):
+        
+class LFUCache(LRUCache):
+    
+    fastCountStorage = dict()
     
     def __init__(self):
-        self.storage = dict()
+        self.fastCountStorage = dict()
     
     @Storage.convert_params
     def put(self, p: Point):
-        self.storage[p] = self.storage.setdefault(p,0)+1
+        
+        node = self.fastAccessStorage.get(p,False)
+        
+        if not node:                
+            self.fastSaveStorage.append(p)
+            self.fastAccessStorage[p] = self.fastSaveStorage.last
+            self.fastCountStorage[self.fastSaveStorage.last] = 1
+        else:
+            self.fastCountStorage[node] += 1
      
-    def get(self, n):
-        return sorted(self.storage.items(),key=lambda kv:(kv[1],kv[0]),reverse = True)[:n]     
-    
-    
-    
-    
+    def getTopNUsed(self, n):
+        return sorted(self.fastCountStorage.items(),key=lambda r: r[1],reverse = True)[:n]     
     
     
     
 # --------------------------
-p1 = Point(13,13,13) # custom point (not from range put)
-p2 = Point(2,11,101) # point from range put
+p1 = Point(13,13,13)
+p2 = Point(2,11,101) 
 
 lru = LRUCache()
 lfu = LFUCache()
-lfu.put(p1)
+
+lru.put(p2)
+lru.put(13,13,13)
+lfu.put(p2)
 lfu.put(13,13,13)
 
-for i in range(10):  # range put
+for i in range(5):  # range put
     lru.put(1+i, 10+i, 100+i)
+    lfu.put(1+i, 10+i, 100+i)
 
-lru.get(p1) # get custom not-existing point
-lru.get(p2) # get existing point from range put
 
-lfu.storage
+    lru.get(p1) 
+lru.get(p2)
+
+lfu.getTopNUsed(3)
